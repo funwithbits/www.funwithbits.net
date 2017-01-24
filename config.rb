8f5e00d1be19d88f9ff3055e7c0eac641f70731b
Raphael Carvalho <raphaelsc@scylladb.com>
2017-01-24 02:10:59 -0200

    fix config.rb

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/www.funwithbits.net/"
-http_images_path = "/www.funwithbits.net/images"
+http_path = "/"
+http_images_path = "/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/www.funwithbits.net/fonts"
-css_dir = "public/www.funwithbits.net/stylesheets"
+http_fonts_path = "/fonts"
+css_dir = "public/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
